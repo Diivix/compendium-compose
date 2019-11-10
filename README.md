608560a5acbce6ccd9cd09ac63263ffe29725021
Chris <chrisiivix@gmail.com>
2019-11-10 20:05:01 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Compendium Compose
+
+Docker compose project for the Compendium project.
+
+1. Run `./setup.sh`
---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+TICKETBOOTH_DATA=/data/ticketbooth
+
+mkdir -p $TICKETBOOTH_DATA/keys
+openssl genrsa -out $TICKETBOOTH_DATA/keys/private.key 2048
+openssl rsa -in $TICKETBOOTH_DATA/keys/private.key -outform PEM -pubout -out $TICKETBOOTH_DATA/keys/public.key
+# openssl req -new -newkey rsa:2048 -days 365 -nodes -x509 -subj "/C=AU/ST=ACT/L=Canberra/O=OrangeLightning/CN=ticketbooth" -keyout $TICKETBOOTH_DATA/keys/server.key -out $TICKETBOOTH_DATA/keys/server.crt
+
+echo -n "Registry username": 
+read -s username
+echo -n "Registry password": 
+read -s password
+
+docker login registry.gitlab.com -u $username -p $password
+docker-compose up -d

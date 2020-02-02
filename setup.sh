cd1d3a4312bda5163564a91612cd9d28d6c19fc8
Chris <chrisiivix@gmail.com>
2020-02-02 02:14:26 +0000

    Fixed setup file.

--- setup.sh
+++ setup.sh
@@ -8,15 +8,19 @@ docker rmi $(docker images -a -q)
 
 rm -r $TICKETBOOTH_DATA $COMPENDIUM_API_DATA
 mkdir -p $TICKETBOOTH_DATA/keys
-mkdir -p $TICKETBOOTH_DATA
+mkdir -p $COMPENDIUM_API_DATA/keys
 
+# Create keys for ticketbooth
 openssl genrsa -out $TICKETBOOTH_DATA/keys/private.key 2048
 openssl rsa -in $TICKETBOOTH_DATA/keys/private.key -outform PEM -pubout -out $TICKETBOOTH_DATA/keys/public.key
 # openssl req -new -newkey rsa:2048 -days 365 -nodes -x509 -subj "/C=AU/ST=ACT/L=Canberra/O=OrangeLightning/CN=ticketbooth" -keyout $TICKETBOOTH_DATA/keys/server.key -out $TICKETBOOTH_DATA/keys/server.crt
 
-echo -n "Registry username": 
+# Copy public keys to compendium-api
+cp $TICKETBOOTH_DATA/keys/public.key $COMPENDIUM_API_DATA/keys/public.key
+
+echo -n "GitLab Docker Registry username": 
 read -s username
-echo -n "Registry password": 
+echo -n "GitLab Docker Registry password": 
 read -s password
 
 docker login registry.gitlab.com -u $username -p $password
@@ -25,9 +29,11 @@ docker-compose up -d
 sleep 10
 
 # Ticketbooth database
-docker exec -t --workdir /app ticketbooth node_modules/.bin/sequelize db:migrate
-docker exec -t --workdir /app ticketbooth node_modules/.bin/sequelize db:seed:all
+echo "Setting up ticketbooth database..."
+docker exec -t --workdir /app ticketbooth npx sequelize-cli db:migrate
+docker exec -t --workdir /app ticketbooth npx sequelize-cli db:seed:all
 
 # Compendium API database
-docker exec -t --workdir /app compendium-api node_modules/.bin/sequelize db:migrate
-docker exec -t --workdir /app ompendium-api node_modules/.bin/sequelize db:seed:all
+echo "Setting up compendium-api database..."
+docker exec -t --workdir /app compendium-api npx sequelize-cli db:migrate
+docker exec -t --workdir /app compendium-api npx sequelize-cli db:seed:all
